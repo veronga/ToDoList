e992084a33c0ad22978de52893f1314d480cd3ae
atlonis <veronika.belozechko@gmail.com>
2020-03-20 15:45:51 +0300

    fix

--- src/components/Tasks/AddTaskForm.jsx
+++ src/components/Tasks/AddTaskForm.jsx
@@ -7,31 +7,37 @@ const AddTaskForm = ({ list, onAddTask }) => {
   const [visibleForm, setFormVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState("");
+  const [error, setError] = useState("");
 
   const toggleFormVisible = () => {
     setFormVisible(!visibleForm);
     setInputValue("");
+    setError("");
   };
 
   const addTask = () => {
-    const obj = {
-      listId: list.id,
-      text: inputValue,
-      completed: false
-    };
-    setIsLoading(true);
-    axios
-      .post("http://localhost:3001/tasks", obj)
-      .then(({ data }) => {
-        onAddTask(list.id, data);
-        toggleFormVisible();
-      })
-      .catch(e => {
-        alert("Ошибка при добавлении задачи!");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    if (inputValue === "") {
+      setError("Пожалуйста введите текст задачи");
+    } else {
+      const obj = {
+        listId: list.id,
+        text: inputValue,
+        completed: false
+      };
+      setIsLoading(true);
+      axios
+        .post("http://localhost:3001/tasks", obj)
+        .then(({ data }) => {
+          onAddTask(list.id, data);
+          toggleFormVisible();
+        })
+        .catch(e => {
+          alert("Ошибка при добавлении задачи!");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
   };
 
   return (
@@ -56,6 +62,7 @@ const AddTaskForm = ({ list, onAddTask }) => {
           <button onClick={toggleFormVisible} className="button button--grey">
             Отмена
           </button>
+          <span className="tasks__text-error">{error}</span>
         </div>
       )}
     </div>
